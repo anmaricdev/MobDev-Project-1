04ed6c417a58d858d0caefd0ba7a3c792b8bfa0e
Ante Maric <49611582+anmaricdev@users.noreply.github.com>
2022-05-15 05:34:48 +0200

    Testing app 1
    final tests to see if everything works

--- app/src/main/java/dec/hex/MainActivity.kt
+++ app/src/main/java/dec/hex/MainActivity.kt
@@ -23,14 +23,11 @@ class MainActivity : AppCompatActivity() {
     // dialog when result is correct
     private lateinit var dialog: AlertDialog.Builder
 
-    // 0, ..., 3 for EditText on reading direction
-    private var checkStep: Int = 0
 
     // the onchange listener for all input fields
     private var textWatcher: TextWatcher = object : TextWatcher {
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            if (checkStep == 0 && hexInput.hasFocus() && s.toString() == hexCorrect) {
-                //checkStep = 1
+            if (hexInput.hasFocus() && s.toString() == hexCorrect) {
                 dialog.show()
                 //continue with next task
                 createTask()
@@ -45,7 +42,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     // for task and result check
-    private var number: Float = 0f
+    private var number: Int = 0
     private var hexCorrect: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -74,37 +71,39 @@ class MainActivity : AppCompatActivity() {
         hexInput.setText("")
 
         //create new task, first create random number
-        val minExp = -4
-        val maxExp = 5
-        val parts = List(maxExp - minExp + 1) { 2f.pow(maxExp - it) }
+        val minExp = 1
+        val maxExp = 1
+        val parts = List(maxExp - minExp + 2) { 16f.pow(maxExp-it) }
         Log.v(TAG, "List with values: " + parts.toString())
-        val randVal = MutableList(maxExp - minExp + 2) { (0..1).random() }
+        val randVal = MutableList(maxExp - minExp + 2) { (0..15).random() }
         Log.v(TAG, "Random values: " + randVal.toString())
-        number = 0f
+        number = 0
         for (i in 0..(maxExp - minExp)) {
-            number += parts[i] * randVal[i]
+            number += parts[i].toInt() * randVal[i]
+            Log.v(TAG, "parts: " + parts.toString())
+            Log.v(TAG, "randVal: " + randVal.toString())
         }
 // section for testing: just for one case: 0f and check whether it is changed
         // number = 0f // ok for -0.5, -0.125, 2, 8, -32, 0.0625
 // end section for testing
         // check whether number is still zero, if so change it
-        if (number == 0f) { //attention: in general it is not safe to use == for comparison of floats!
+        if (number == 0) { //attention: in general it is not safe to use == for comparison of floats!
             val randNum = (0..(maxExp - minExp)).random()
             randVal[randNum] = 1
-            number = parts[randNum]
+            number = parts[randNum].toInt()
         }
         // finalize number calculation (sign)
-        if (randVal[maxExp - minExp + 1] == 1) {
+        if (randVal[maxExp - minExp + 1] == (0..15).random()) {
             number = -number
         }
 // section for testing: set other numbers here
         //number = 63.9375f // ok
         //number = -63.9375f // ok
-        //number = 1f // ok
+        //number = 127 // ok
         //number = -1f // ok
         //number = 0.5f // ok
         //number = -0.5f // ok
-        number = -14.5f // ok
+        //number = -14.5f // ok
         //number = -0.0625f // ok
         //number = -34.125f // ok
         //number = 55.5f // ok
@@ -112,28 +111,29 @@ class MainActivity : AppCompatActivity() {
         //number = 22.0625f // ok
 // end section for testing
         // now calculate correct solutions, based on decimal to hexadecimal conversion
-        /* if (number >= 0) {
-             hexCorrect = "+1"
-         }
-         else {
-             hexCorrect = "-1"
-         }
-        */
+        if (number >= 0) {
+            hexCorrect = "+" + (String.format("%X", number))
+        } else {
+            hexCorrect = (String.format("%X", number))
+            hexCorrect = "-" + trimHexBits(hexCorrect)
+        }
         Log.v(TAG, "Number: " + number.toString())
-        try {
-            //hexCorrect = Integer.toHexString(number)
-            hexCorrect = (String.format("%x", number))
-            Log.v(TAG, "Hexadecimal representation: " + hexCorrect)
-        } catch (exception : Exception){println(exception.toString())}
+        //hexCorrect = (String.format("%X", number))
+        //hexCorrect = Integer.toHexString(number)
+        Log.v(TAG, "Hexadecimal representation: " + hexCorrect)
 
 
         // put new task to UI
         taskView.text = number.toString()
 
-        // start checks with first step
-        checkStep = 0
-
         // set focus on first input field
         hexInput.requestFocus()
+    }
+
+    // getting rid of the first 6 F digits that come while converting from a negative decimal number to hex
+    // e.g before: (decimal) -144 -> (hex) FFFFFF70 | after:(decimal) -144 -> (hex) -70
+    private fun trimHexBits(s: String) : String{
+        val str = s.trimStart(){it == 'F'}
+        return str
     }
 }
